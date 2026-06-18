706538cb6ae07d2851a5874d36ec532ff2cc5787
BlueChest <palmtree414@gmail.com>
2026-06-18 17:23:07 +0900

    크롤러 붙이기전 메인

---
+++ main.py
@@ -0,0 +1,108 @@
+import streamlit as st
+
+#레이아웃
+#헤더
+st.set_page_config(page_title="채용공고 자동 크롤링 서비스",
+                   layout='wide')
+
+st.title("채용 공고 조회 서비스")
+st.write(""" 이 서비스는 """)
+
+
+site_select = st.radio ("크롤링할 사이트 선택",
+                        ["사람인","고용24"],
+                        horizontal=True)
+
+#with st.expander("열어보세요",expanded=True):
+#    st.write("확장")
+with st.expander("상세 검색 조건"):
+    #여기서 두개로 쪼개는 역할
+    col1,col2 = st.columns(2)
+
+    with col1:
+        st.success('컬럼 1')
+        search_text=st.text_input("검색어를 입력",
+                                  placeholder="예 : 파이썬, 인공지능")
+        
+        st.success('컬럼 2')
+        search_text=st.text_input("제외할 검색어",
+                                    placeholder="예 : 야간 근무, 출장")
+
+        max_pages = st.number_input('크롤링 페이지 수',
+                                    max_value=10,
+                                    min_value=1)
+
+                        
+    
+
+    with col2:
+        if site_select == '사람인':
+            #사람인 사이트에서 지역 의미하는 코드
+            loc_options = {
+                "전체": None,
+                "서울": "101000",
+                "경기": "102000",
+                "인천": "108000",
+                "부산": "106000",
+                "대구": "104000",
+                "광주": "103000",
+                "대전": "105000"
+            }
+            #딕셔너리 형상인 옵션에서 KEY만 추출해서 리스트로 만들기
+            selected_location=st.multiselect('지역을 선택하세요',
+                           list(loc_options.keys()),
+                           default=['전체'])
+            
+            #st.write(selected_location)
+            #크롤링 진행할때 사용하기 위해서 문자열 추출 0:"전체" 1:"서울" 2:" 인천" 바로 사용을 못함 101000 식으로 나와야하니까
+
+            locations = [loc_options[x] for x in selected_location if loc_options[x]]
+            #st.write(locations)
+            #이걸 하면 101000 식으로 나오게 됨
+            # x 는 서울 경기 부산같은 지역명
+            # if 조건 붙는 이유 지역명이 없는 지역이라면 추가 안됨
+
+            #직무 검색
+            cat_options = {
+                "전체": None,
+                "IT개발·데이터": "2",
+                "경영·사무": "3",
+                "마케팅·홍보": "4",
+                "디자인": "9",
+                "영업": "5"
+            }
+
+            selected_category = st.multiselect("직무를 선택하세요.",
+                                               list(cat_options.keys()),
+                                               default=['IT개발·데이터'])
+            
+            category = [cat_options[x] for x in selected_location if cat_options[x]]
+
+            #경력
+            career_option = {'전체':'0','신입':'1','경력':'2','신입/경력':'3',}
+            selected_career = st.selectbox('경력을 선택하세요',list(career_option.keys()))
+            career = career_option[selected_career]
+            #st.write(career)
+
+            #학력
+            edu_option = {'전체':'0','고졸':'1','대졸(2,3년)':'2','대졸(4년)':'3','석사':'4','박사':'5'}
+            selected_edu = st.selectbox("학력을 선택하시오",list(edu_option.keys()))
+            edu = edu_option[selected_edu]
+
+        else:
+             #지역, 직무,경력,학력
+             region = st.text_input('지역 코드를 입력하세요',value='11000',
+                                    help='지역 코드 알 수 없는 관계로 서울로 제한')
+             
+             ocupation = st.text_input('직종 코드를 입력하세요',value='024',
+                                       help='직종 코드 제한')
+             
+             career_options = {"전체": "A", "신입": "N", "경력": "E", "관계없음": "Z"}
+             edu_options = {"전체": "noEdu", "중졸이하": "01,02", "고졸": "03", "대졸(2~3년)": "04", "대졸(4년)": "05", "석사": "06", "박사": "07", "학력무관": "00"}
+             career = st.selectbox("경력을 선택하세요",list(career_options.keys()))
+             career = career_options[career]
+             edu = st.selectbox("학력을 선택하세요",list(edu_options.keys()))
+             edu = edu_options[edu]
+
+
+
